21fd2c316e037dd0a0ab5cca211cf37d746ac625
Mohammed <arrahmaan9901@gmail.com>
2022-01-21 15:37:38 +0530

    Output shown in screenshot

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+import Header from "./Header";
+
+export default function Dashboard(){
+    return(
+        <>
+        <Header/>
+        <Button/>
+        </>
+    );
+}
