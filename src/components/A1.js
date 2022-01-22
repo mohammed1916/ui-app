9c3755ddb5d345398dafb09c6f6a5df89805473a
Mohammed <arrahmaan9901@gmail.com>
2022-01-22 08:29:37 +0530

    output at screenshot

---
+++ src/components/A1.js
@@ -0,0 +1,21 @@
+export default function A1(props){
+    function computeGreater(a,b,c){
+        var greatest = 0;
+        if (a> b & a>c){
+            greatest = a
+        } else if (b>c){
+            greatest = b
+        } else {
+            greatest = c
+        }
+        return greatest
+    }
+
+    return(
+        <center>
+            <h2>
+                {computeGreater(props.a,props.b,props.c)}
+            </h2>
+        </center>
+    );
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
-import HelloWorld from "./HelloWorld";
+import A2 from "./A2";
 
-export default function App(){
-    return(
-        <HelloWorld/>
+export default function App() {
+    return (
+        <>
+            <A2 />
+        </>
     );
 }
