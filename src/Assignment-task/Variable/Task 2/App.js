48396279b38a3863f1623e0145483153bc215809
Mohammed <arrahmaan9901@gmail.com>
2022-01-23 15:42:34 +0530

    output at screenshot

---
+++ src/Assignment-task/Variable/Task 2/App.js
@@ -0,0 +1,12 @@
+import Sample from "../Task 2/Sample";
+
+export default function App(){
+    
+    return(
+        <>
+            <Sample a={5} b={5} c={5}/>
+            <Sample a={10} b={10} c={10}/>
+            <Sample a={20} b={20} c={20}/>
+        </>
+);
+}
---
+++ src/Assignment-task/Variable/Task 2/Sample.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param a integer to add 
+ * @param b integer to add 
+ * @param c integer to add 
+ * @returns sum of param
+ */
+export default function Sample({a,b,c}){
+    
+    return(
+        <>
+            <h2>
+                {a+b+c}
+            </h2>
+        </>
+);
+}
