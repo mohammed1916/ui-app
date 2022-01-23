64be81f619987db5ce9b1fa3fa199d08b1892a2e
Mohammed Abdullah <63586305+mohammed1916@users.noreply.github.com>
2022-01-23 15:59:55 +0530

    Task 3 from `variable` assignment (#6)
    * output at screenshot
    
    * output at screenshot

---
+++ src/Assignment-task/Variable/Task 3/Z.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param a1 integer to add 
+ * @param b1 integer to add 
+ * @param c1 integer to add
+ * @param d1 integer to add
+ * @returns sum of params 
+ */
+ export default function X({a1,b1,c1,d1}){
+    
+    return(
+        <>
+            <h2>
+                {a1+b1+c1+d1}
+            </h2>
+        </>
+);
+}
