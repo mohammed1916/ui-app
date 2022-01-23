301a2b9c83d41a0963800825be70119f3694186d
Mohammed <arrahmaan9901@gmail.com>
2022-01-23 15:56:06 +0530

    output at screenshot

---
+++ src/Assignment-task/Variable/Task 3/App.js
@@ -0,0 +1,14 @@
+import X from "./X";
+import Y from "./Y";
+import Z from "./Z";
+
+export default function App(){
+    
+    return(
+        <>
+            <X a={1} b={1}/>
+            <Y a={1} b={1} c={1} />
+            <Z a1={1} b1={1} c1={1} d1={1} />
+        </>
+);
+}
---
+++ src/Assignment-task/Variable/Task 3/Y.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param a integer to add 
+ * @param b integer to add
+ * @param c integer to add 
+ * @returns sum of params 
+ */
+ export default function X({a,b,c}){
+    
+    return(
+        <>
+            <h2>
+                {a+b+c}
+            </h2>
+        </>
+);
+}
